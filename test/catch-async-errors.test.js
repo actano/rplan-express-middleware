4ef687562d8b2deecf8c4b66087e5985ee22346a
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2019-04-03 16:22:26 +0200

    RX-16099: Add test for catch async errors

--- test/catch-async-errors.test.js
+++ test/catch-async-errors.test.js
@@ -1,5 +1,45 @@
+import chai from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+import { catchAsyncErrors } from '../src/catch-async-errors'
+
+chai.use(sinonChai)
+const { expect } = chai
+
 describe('catch-async-errors', () => {
-  it('should catch an error', () => {
-    // TODO
+  it('should catch an error', async () => {
+    const requestStub = sinon.stub()
+    const responseStub = sinon.stub()
+    const nextStub = sinon.stub()
+
+    const catchAsyncErrorsHandler = catchAsyncErrors(async () => {
+      throw new Error('foo')
+    })
+
+    await catchAsyncErrorsHandler(requestStub, responseStub, nextStub)
+
+    expect(nextStub).to.have.callCount(1)
+
+    const nextArg = nextStub.getCall(0).args[0]
+    expect(nextArg).to.be.instanceOf(Error)
+    expect(nextArg.message).to.be.equal('foo')
+  })
+
+  it('should call next without args on no error', async () => {
+    const requestStub = sinon.stub()
+    const responseStub = sinon.stub()
+    const nextStub = sinon.stub()
+
+    const catchAsyncErrorsHandler = catchAsyncErrors(async () => {
+      // do nothing
+    })
+
+    await catchAsyncErrorsHandler(requestStub, responseStub, nextStub)
+
+    expect(nextStub).to.have.callCount(1)
+
+    const nextArg = nextStub.getCall(0).args[0]
+    expect(nextArg).to.equal(undefined)
   })
 })
