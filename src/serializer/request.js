2a8a061ae697251d7a04bb23eda8e8cc1768b908
thrucker <t.rucker@itsonix.eu>
2020-09-07 10:28:47 +0200

    Ensure `route.path` to be a string

--- src/serializer/request.js
+++ src/serializer/request.js
@@ -2,7 +2,7 @@ import flow from 'lodash/fp/flow'
 import omit from 'lodash/fp/omit'
 import pick from 'lodash/fp/pick'
 import update from 'lodash/fp/update'
-
+import toString from 'lodash/toString'
 
 const REQUEST_PROPERTY_WHITELIST = [
   'borderContext',
@@ -27,6 +27,12 @@ const requestSerializer = flow(
     'cookie',
     'host',
   ])),
+  // `route.path` may be a RegEx which may cause trouble when parsing the log message. So we always
+  // convert it to a string.
+  update(
+    'route.path',
+    toString,
+  ),
 )
 
 export {
